48999591fd66c733ad45951cd58b4988c3a52e17
Daniel Bae <danibae91@icloud.com>
2021-03-10 23:21:07 -0800

    add functionality to app

--- movies.js
+++ movies.js
@@ -1,1 +1,39 @@
 // Movies App (jQuery) 
+
+// function htmlTableData(arg) 
+function htmlTableData(movieData) {
+    return `
+        <tr>
+            <td>${movieData.title}</td>
+            <td>${movieData.rating}</td>
+            <td><button class="btn-del">Delete</button>
+        </tr>
+    `
+}; 
+
+// #movie-form
+$('#movie-form').on('submit', function(e) {
+    let title = $('#title').val(); 
+    let rating = $('#rating').val(); 
+
+    // movieData -> title, rating
+    let movieData = { title, rating }
+    let htmlData = htmlTableData(movieData); 
+
+    // tbody
+    $('tbody').append(htmlData); 
+
+    // reset input behavior 
+    $('#title').val(''); 
+    $('#rating').val(''); 
+
+    // preventDefault behavior
+    e.preventDefault(); 
+
+})
+
+// delete button 
+$('tbody').on('click', '.btn-del', function(e) {
+    // .closest(tr) method to remove entire tr 
+    $(e.target).closest('tr').remove(); 
+}); 
